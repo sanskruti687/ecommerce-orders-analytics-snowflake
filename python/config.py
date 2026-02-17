bc87847659f50a7f76656240834183ac91e90c21
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2026-02-17 14:09:31 +0530

    modify file

---
+++ python/config.py
@@ -0,0 +1,8 @@
+SNOWFLAKE_CONFIG = {
+    "user": "Sanskruti29",
+    "password": "Sanskruti@1234",
+    "account": "YOUR_ACCOUNT",
+    "warehouse": "COMPUTE_WH",
+    "database": "SHOP_DW",
+    "schema": "RAW"
+}
---
+++ python/insert_orders.py
@@ -0,0 +1,29 @@
+import snowflake.connector
+import pandas as pd
+
+conn = snowflake.connector.connect(
+    user="YOUR_USER",
+    password="YOUR_PASSWORD",
+    account="YOUR_ACCOUNT",
+    warehouse="COMPUTE_WH",
+    database="SHOP_DB",
+    schema="RAW"
+)
+
+cursor = conn.cursor()
+
+df = pd.read_csv("data/orders.csv")
+
+for _, row in df.iterrows():
+    query = f"""
+    INSERT INTO ORDERS_RAW
+    VALUES ({row.ORDER_ID}, '{row.CUSTOMER_NAME}',
+            '{row.PRODUCT_NAME}', {row.QUANTITY},
+            {row.PRICE}, '{row.ORDER_DATE}');
+    """
+    cursor.execute(query)
+
+print("Data inserted successfully")
+
+cursor.close()
+conn.close()
