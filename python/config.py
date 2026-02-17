de60872645c3a0a47ae7fce68a2628a358e07bed
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2026-02-17 14:18:18 +0530

    add account name

--- python/config.py
+++ python/config.py
@@ -1,7 +1,7 @@
 SNOWFLAKE_CONFIG = {
     "user": "Sanskruti29",
     "password": "Sanskruti@1234",
-    "account": "YOUR_ACCOUNT",
+    "account": "ZJLIMSR-GY00818",
     "warehouse": "COMPUTE_WH",
     "database": "SHOP_DW",
     "schema": "RAW"
